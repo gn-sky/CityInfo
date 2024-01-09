8e004ca9f96365f5b0cb929d3570713103801abc
Eugene Sky <eugene.sky.dev@gmail.com>
2024-01-08 22:17:41 -0600

    CitiesController starter

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            return Ok(new List<object>
+            {
+                new { id = 1, Name = "New York City" },
+                new { id = 2, Name = "Antwerpen" }
+            });
+        }
+    }
+}
