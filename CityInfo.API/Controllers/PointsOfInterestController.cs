a7421080d7ae79b06f2cd7edbce0e4c5f69be6f9
Eugene Sky <eugene.sky.dev@gmail.com>
2024-01-09 19:41:57 -0600

    PointOfInterest delete endpoint

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -101,6 +101,23 @@ namespace CityInfo.API.Controllers
             pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
 
             return NoContent();
-        }   
+        }
+
+        [HttpDelete("{pointOfInterestId}")]
+        public ActionResult DeletePointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+            return NoContent();
+        }
     }
 }
